75e61fa0798d1271c46ce737e050db0b9c2b78e3
MihailK <mihailkopitov@mail.ru>
2025-10-28 18:17:30 +0500

    HomeWork completed

--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -13,40 +13,40 @@ public class NumberValidatorTests
     public void Constructor_Should_Throw_For_InvalidParameters(int precision, int scale, bool onlyPositive)
     {
         var action = () => new NumberValidator(precision, scale, onlyPositive);
-        
+
         action.Should().Throw();
     }
-    
+
     [TestCase(5, 2, false)]
     [TestCase(10, 5, true)]
     [TestCase(10, 0, false)]
     public void Constructor_Should_NotThrow_For_ValidParameters(int precision, int scale, bool onlyPositive)
     {
         var action = () => new NumberValidator(precision, scale, onlyPositive);
-        
+
         action.Should().NotThrow();
     }
-    
-    [TestCase(17,2,true,null, false)]
-    [TestCase(17,2,true,"", false)]
-    [TestCase(17,2,true,"a.1d", false)]
-    [TestCase(2,1,true,"+0.0", false)]
-    [TestCase(2,1,true,"00.0", false)]
-    [TestCase(3,1,true,"0.00", false)]
-    [TestCase(17,2,true,"-0.0", false)]
-    
-    [TestCase(17,2,true,"123.45", true)]
-    [TestCase(17,2,true,"123,45", true)]
-    [TestCase(17,2,true,"+0.0", true)]
-    [TestCase(17,2,false,"-0.0", true)]
-    [TestCase(17,0,true,"0", true)]
-    [TestCase(17,0,false,"-10", true)]
-    public void IsValidNumber_Should_ReturnExpectedResults(int precision, int scale, bool onlyPositive, string value, bool expectedResult)
+
+    [TestCase(17, 2, true, null, false)]
+    [TestCase(17, 2, true, "", false)]
+    [TestCase(17, 2, true, "a.1d", false)]
+    [TestCase(2, 1, true, "+0.0", false)]
+    [TestCase(2, 1, true, "00.0", false)]
+    [TestCase(3, 1, true, "0.00", false)]
+    [TestCase(17, 2, true, "-0.0", false)]
+    [TestCase(17, 2, true, "123.45", true)]
+    [TestCase(17, 2, true, "123,45", true)]
+    [TestCase(17, 2, true, "+0.0", true)]
+    [TestCase(17, 2, false, "-0.0", true)]
+    [TestCase(17, 0, true, "0", true)]
+    [TestCase(17, 0, false, "-10", true)]
+    public void IsValidNumber_Should_ReturnExpectedResults(int precision, int scale, bool onlyPositive, string value,
+        bool expectedResult)
     {
         var validator = new NumberValidator(precision, scale, onlyPositive);
-        
+
         var actualResult = validator.IsValidNumber(value);
-    
+
         actualResult.Should().Be(expectedResult);
     }
 }
